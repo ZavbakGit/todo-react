409bc74ba3f08784e5d8596e87947889c5accd37
ZavbakGit <f%jtVPW3hbmjy!>
2021-05-13 14:51:32 +1000

    Start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello world')
+import  React from 'react'
+import  ReactDOM from 'react-dom'
+
+const el = <h1>Hello world!</h1>
+
+ReactDOM.render(el,document.getElementById('root'))
+
+console.log(el)
