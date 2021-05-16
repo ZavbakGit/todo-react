dc731b37ed71c31124358444e210baf9d1c0ed58
ZavbakGit <f%jtVPW3hbmjy!>
2021-05-16 10:24:48 +1000

    StartStart

--- src/index.js
+++ src/index.js
@@ -1,40 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AppHeader from "./components/app-header";
+import SearchPanel from "./components/search-panel";
+import TodoList from "./components/todo-list";
 
-const TodoList = () => {
-    const items = ['Learn', 'Build Awesome App']
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    )
-}
-
-const AppHeader = () => {
-    return (
-        <h1>My Todo List</h1>
-    )
-}
-
-const SearchPanel = () => {
-    const searchText = 'Type here search'
-    const searchStyle = {
-        fontSize:'20px'
-    }
-    return <input
-        style={searchStyle}
-        placeholder={searchText}/>
-}
 
 const App = () => {
+    const todoData = [
+        {label:'Drink Coffee', important: false},
+        {label:'Make Awesome App', important: true},
+        {label:'Have a lunch', important: false},
+    ]
+
+
     return <div>
         <AppHeader/>
         <SearchPanel/>
-        <TodoList/>
+        <TodoList todos = {todoData}/>
     </div>
 }
 
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(<App/>,
+    document.getElementById('root'))
 
